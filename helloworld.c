2b95a3f84eca54f35c74cd93449360b8e57a8294
josemichaelrubio <contact@josemichaelrubio.com>
2025-09-18 07:05:30 -0400

    pracitice user inpit

--- helloworld.c
+++ helloworld.c
@@ -1,4 +1,5 @@
 #include <stdio.h> // Told C what we use lib to use
+#define MAX_LEN 10 // here, we are doing a max length for user input, make sure to leave room for the /0
 // Single line comment
 /*
 
@@ -16,6 +17,15 @@ int main(void){
     char string_name_example[] = "BOB" ; // so a string is treated like an array, you can assign it any number of elements, you can also leave it blank and the last value is a \O
     printf("%s\n",string_name_example); // notice %s
     printf("%c\n",string_name_example[3]); // notice %c
+    // demonstration of user input
+    char buffer[MAX_LEN]; // buffer is the name we name a user input
+    int some_number = 0;
+    printf("Type numbers, this is a user input example with scannff, MAX 9 DIGITS PLZ: \n");
+    fgets(buffer,MAX_LEN,stdin); // get user input
+    sscanf(buffer,"%d", &some_number); //convert to int
+    printf("This is what you typed: %d", some_number);
+
+
     return 0;
 }
 
